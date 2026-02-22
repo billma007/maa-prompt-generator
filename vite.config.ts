ebac1de284680d81a74319081b9fe6db5114dcb9
billma007 <maboning237103015@163.com>
2026-02-22 12:11:50 +0800

    Configure for Netlify deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,5 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/maa-prompt-generator/', // Replace with your repository name if different
   plugins: [vue()],
 })
